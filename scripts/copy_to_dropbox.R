ed01dc3d5697ec898e4ae05fafd0c219c24757ef
Md. Mehedi Hasan Khan <mhkhan27@users.noreply.github.com>
2020-03-30 12:08:18 +0600

    initial commit

---
+++ scripts/copy_to_dropbox.R
@@ -0,0 +1,13 @@
+# common ------------------------------------------------------------------
+
+day_to_run<- Sys.Date()
+
+write_csv_output <- c("yes","no")[1]
+
+# adolescent --------------------------------------------------------------
+
+rmarkdown::render("Edu_facility_Daily_Monitoring_Report.Rmd")
+
+file_location <- "Edu_facility_Daily_Monitoring_Report.html"
+copy_to_drop <- "C:\\Users\\MEHEDI\\Dropbox\\REACH_BGD\\REACH\\Ongoing\\70XXX - UNICEF (Host Community Education)/04_daily_monitoring/01_facility_assessment/02_daily_reports/"
+file.copy(file_location,paste0(copy_to_drop,str_replace_all(day_to_run,"-","_"),"_Education_facility_Daily_Monitoring_Report.html"),overwrite = T)
